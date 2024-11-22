092a32e0a6e6ce43a7bebae3e80efe44881d21ac
jerryzhonathan <jerryzhonathan25>
2024-11-22 23:37:08 +0700

    backroll

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
+const serviceAccount = require("../../serviceAccountKey.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
