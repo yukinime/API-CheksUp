cc71cbbe2cd44111614db42989b072d723152bcb
jerryzhonathan <jerryzhonathan25>
2024-11-22 23:28:20 +0700

    commit firebase

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,5 +1,5 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('../../serviceAccountKey.json');
+const admin = require("firebase-admin");
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
